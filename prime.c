d44e5aa25b5ff7294d9d794988901651fef2ebf5
riteshkoranga <riteshkoranga5@gmail.com>
2024-10-12 14:38:58 +0530

    added new file

---
+++ prime.c
@@ -0,0 +1,22 @@
+//hey this to check the prime numbers
+
+#include <stdio.h>
+#include <math.h>
+
+int isPrime(int num) {
+    if (num <= 1) return 0;
+    for (int i = 2; i <= sqrt(num); i++) {
+        if (num % i == 0) return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int num = 17;  // Change this number to test
+    if (isPrime(num))
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+    return 0;
+}
+
